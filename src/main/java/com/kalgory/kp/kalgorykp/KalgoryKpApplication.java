ed360f26cd63862a80d80fc1c23648edcd0ed991
Wonyeong Choi <coma01155@gmail.com>
2022-01-24 22:13:00 +0900

    Chore: spring boot application 생성(Gradle)

---
+++ src/main/java/com/kalgory/kp/kalgorykp/KalgoryKpApplication.java
@@ -0,0 +1,13 @@
+package com.kalgory.kp.kalgorykp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KalgoryKpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KalgoryKpApplication.class, args);
+	}
+
+}
