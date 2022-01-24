66c8bb3e4e9cef948582709f292ab7925d371bb7
Wonyeong Choi <coma01155@gmail.com>
2022-01-24 22:13:00 +0900

    initial spring-boot setting

---
+++ src/test/java/com/kalgory/kp/kalgorykp/KalgoryKpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kalgory.kp.kalgorykp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KalgoryKpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
