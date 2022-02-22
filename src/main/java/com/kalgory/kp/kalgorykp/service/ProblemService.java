5be5ebcec6299bcb74aec82135fb7dd3fd8525b2
Wonyeong Choi <coma01155@gmail.com>
2022-02-22 23:25:51 +0900

    Refactor: mock data 관련 기능 제거

--- src/main/java/com/kalgory/kp/kalgorykp/service/ProblemService.java
+++ src/main/java/com/kalgory/kp/kalgorykp/service/ProblemService.java
@@ -1,11 +1,7 @@
 package com.kalgory.kp.kalgorykp.service;
 
-import com.kalgory.kp.kalgorykp.dto.ProblemListResponseDto;
 import com.kalgory.kp.kalgorykp.dto.ProblemSaveRequestDto;
-import com.kalgory.kp.kalgorykp.repository.MockProblemRepository;
 import com.kalgory.kp.kalgorykp.repository.ProblemRepository;
-import java.util.List;
-import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -18,7 +14,6 @@ import org.springframework.transaction.annotation.Transactional;
 public class ProblemService {
 
   private final ProblemRepository problemRepository;
-  private final MockProblemRepository mockProblemRepository;
 
   /**
    * 문제 하나 저장하는 Logic.
@@ -30,28 +25,4 @@ public class ProblemService {
   public Long save(ProblemSaveRequestDto problemSaveRequestDto) {
     return problemRepository.save(problemSaveRequestDto.toEntity()).getId();
   }
-
-  /**
-   * 모든 문제 찾는 Logic.
-   *
-   * @return 문제 리스트.
-   */
-  @Transactional(readOnly = true)
-  public List<ProblemListResponseDto> findAllAsc() {
-    return problemRepository.findAll().stream()
-        .map(ProblemListResponseDto::new)
-        .collect(Collectors.toList());
-  }
-
-
-  /**
-   * mocking data 반환하는 Logic.
-   *
-   * @return mocking 된 문제정보 리스트.
-   */
-  @Transactional(readOnly = true)
-  public List<ProblemListResponseDto> findMockData() {
-    return mockProblemRepository.findAll();
-  }
-
 }
