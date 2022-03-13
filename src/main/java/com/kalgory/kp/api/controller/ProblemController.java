13d22bc0167b03a449041ebd1b9f71c8ab49487b
NEM-NE <dlatqdlatq@mju.ac.kr>
2022-03-13 21:03:47 +0900

    Feat: 문제 제출 API 추가
    * 채점 서버가 없는 관계로 스켈레톤 코드만 작성

--- src/main/java/com/kalgory/kp/api/controller/ProblemController.java
+++ src/main/java/com/kalgory/kp/api/controller/ProblemController.java
@@ -1,10 +1,13 @@
 package com.kalgory.kp.api.controller;
 
 import com.kalgory.kp.api.dto.ProblemReceiveResponseDto;
+import com.kalgory.kp.api.dto.ProblemSubmitRequestDto;
 import com.kalgory.kp.api.service.ProblemService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,5 +24,11 @@ public class ProblemController {
   @ResponseStatus(HttpStatus.OK)
   public ProblemReceiveResponseDto receiveProblem(){
     return problemService.receive();
+  }
+
+  @PostMapping("/problems/submit")
+  @ResponseStatus(HttpStatus.ACCEPTED)
+  public void submitProblem(@RequestBody ProblemSubmitRequestDto problemSubmitRequestDto){
+    problemService.submit(problemSubmitRequestDto);
   }
 }
--- src/main/java/com/kalgory/kp/api/service/ProblemService.java
+++ src/main/java/com/kalgory/kp/api/service/ProblemService.java
@@ -1,6 +1,7 @@
 package com.kalgory.kp.api.service;
 
 import com.kalgory.kp.api.dto.ProblemReceiveResponseDto;
+import com.kalgory.kp.api.dto.ProblemSubmitRequestDto;
 import com.kalgory.kp.api.entity.problem.Problems;
 import com.kalgory.kp.api.exception.CustomException;
 import com.kalgory.kp.api.exception.ErrorCode;
@@ -23,4 +24,9 @@ public class ProblemService {
 
     return new ProblemReceiveResponseDto(problems);
   }
+
+  public void submit(ProblemSubmitRequestDto problemSubmitRequestDto) {
+    // 채점 서버 요청
+  }
+
 }
