6e61e4deab3647c6b90e98a90315e3884b1ab307
Wonyeong Choi <coma01155@gmail.com>
2022-04-04 18:20:11 +0900

    Feat: 접근 권한 변경
    인터셉터에서 쿠키-세션으로 접근 권한 설정하기 때문에 변경함.

--- src/main/java/com/kalgory/kp/api/config/SecurityConfiguration.java
+++ src/main/java/com/kalgory/kp/api/config/SecurityConfiguration.java
@@ -34,7 +34,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
   protected void configure(HttpSecurity http) throws Exception {
     http
         .authorizeRequests()
-        .antMatchers("/sign-up").permitAll()
+        .antMatchers("/**").permitAll()
         .anyRequest().authenticated()
         .and()
         .formLogin().disable()
