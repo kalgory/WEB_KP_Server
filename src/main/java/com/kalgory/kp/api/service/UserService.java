cd20875502dc5bb2c8bdc37c63913e04e61cb1a9
Wonyeong Choi <coma01155@gmail.com>
2022-04-06 15:17:36 +0900

    Refactor: 의존 관계 주입 방법 변경 및 메서드 분리

--- src/main/java/com/kalgory/kp/api/service/UserService.java
+++ src/main/java/com/kalgory/kp/api/service/UserService.java
@@ -5,7 +5,7 @@ import com.kalgory.kp.api.entity.user.Users;
 import com.kalgory.kp.api.exception.CustomException;
 import com.kalgory.kp.api.exception.ErrorCode;
 import com.kalgory.kp.api.repository.UsersRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -13,13 +13,11 @@ import org.springframework.stereotype.Service;
  * 유저 관련 서비스 클래스.
  */
 @Service
+@RequiredArgsConstructor
 public class UserService {
 
-  @Autowired
-  private PasswordEncoder passwordEncoder;
-
-  @Autowired
-  private UsersRepository usersRepository;
+  private final PasswordEncoder passwordEncoder;
+  private final UsersRepository usersRepository;
 
   /**
    * 회원가입 로직.
@@ -28,12 +26,20 @@ public class UserService {
    */
   public void signUp(UserSignUpRequestDto userSignUpRequestDto) {
     String email = userSignUpRequestDto.getEmail();
+    validateEmail(email);
 
+    Users user = getUserFrom(userSignUpRequestDto);
+    usersRepository.save(user);
+  }
+
+  private void validateEmail(String email) {
     if (usersRepository.existsByEmail(email)) {
       throw new CustomException(ErrorCode.DUPLICATED_EMAIL);
     }
+  }
+
+  private Users getUserFrom(UserSignUpRequestDto userSignUpRequestDto) {
     String encodedPassword = passwordEncoder.encode(userSignUpRequestDto.getPassword());
-    Users user = Users.of(userSignUpRequestDto, encodedPassword);
-    usersRepository.save(user);
+    return Users.of(userSignUpRequestDto, encodedPassword);
   }
 }
