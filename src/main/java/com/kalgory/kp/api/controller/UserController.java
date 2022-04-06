058132117fa58d21ffeea2d7cf825db75b777ba5
Wonyeong Choi <coma01155@gmail.com>
2022-04-06 15:16:59 +0900

    Refactor: `@RequestBody` 어노테이션 명시

--- src/main/java/com/kalgory/kp/api/controller/UserController.java
+++ src/main/java/com/kalgory/kp/api/controller/UserController.java
@@ -5,6 +5,7 @@ import com.kalgory.kp.api.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,7 +20,7 @@ public class UserController {
 
   @PostMapping("/sign-up")
   @ResponseStatus(HttpStatus.CREATED)
-  public void registerUser(UserSignUpRequestDto userSignUpRequestDto) {
+  public void registerUser(@RequestBody UserSignUpRequestDto userSignUpRequestDto) {
     userService.signUp(userSignUpRequestDto);
   }
 }
